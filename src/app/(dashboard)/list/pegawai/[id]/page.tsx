9f6d21676bf1eceda546c314312995821f6b15e4
Mosagol <bayu.laksmana14@gmail.com>
2025-01-30 21:51:15 +0700

    Page Profil Update

--- src/app/(dashboard)/list/pegawai/[id]/page.tsx
+++ src/app/(dashboard)/list/pegawai/[id]/page.tsx
@@ -1,119 +1,99 @@
+import BigCalendar from "@/components/BigCalendar";
 import Image from "next/image";
-import { ImageResponse } from "next/server";
+import Link from "next/link";
 
 const SinglePegawaiPage = () => {
-  return (
-    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
-      {/* Bagian Kiri */}
-      <div className="w-full xl:w-2/3">
+return (
+<div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+    {/* Bagian Kiri */}
+    <div className="w-full xl:w-2/3">
         {/* Profil Bagian Atas */}
         <div className="flex flex-col lg:flex-row gap-4">
-          {/* User Info Card */}
-          <div className="bg-lamaSky py-6 px-4 rounded-md flex flex-1 gap-4">
-            <div className="w-1/3">
-              <Image
-                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
-                alt=""
-                width={144}
-                height={144}
-                className="w-36 h-36 rounded-full object-cover"
-              />
-            </div>
-            <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1 className="text-xl font-semibold">Roberto Agus</h1>
-              <p className="text-sm text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-                eaque!
-              </p>
-              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image src="/blood.png" alt="" width={14} height={14} />
-                  <span>A+</span>
+            {/* User Info Card */}
+            <div className="bg-lamaSky py-6 px-2 rounded-md flex flex-1 gap-4">
+                <div className="w-1/3">
+                    <Image
+                        src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                        alt="" width={130} height={10} className="w-36 h-36 rounded-full object-cover" />
                 </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image src="/date.png" alt="" width={14} height={14} />
-                  <span>Januari 2025</span>
+                <div className="w-2/3 flex flex-col justify-between gap-2">
+                    <h1 className="text-xl font-semibold">Roberto Agus</h1>
+                    <p className="text-sm text-gray-700 mb-2">
+                        Technical Support Grade II <br />
+                        Ops Daily Activity Monitoring System
+                    </p>
+                    <div className="flex items-center justify-between gap-1 flex-wrap text-xs font-medium">
+                        <div className="w-full md:w-2/3 lg:w-full 2xl:w-2/3 flex flex-nowrap items-center gap-2">
+                            <Image src="/date.png" alt="" width={14} height={14} />
+                            <span>12 Januari 2025</span>
+                        </div>
+                        <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                            <Image src="/mail.png" alt="" width={14} height={14} />
+                            <span>roberto.agus@iconpln.co.id</span>
+                        </div>
+                        <div className="w-full md:w-2/3 lg:w-full 2xl:w-2/3 flex items-center gap-2">
+                            <Image src="/phone.png" alt="" width={14} height={14} />
+                            <span>+62 852 9833</span>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image src="/mail.png" alt="" width={14} height={14} />
-                  <span>user@gmail.com</span>
+            </div>
+            {/* User Info Small Card */}
+            <div className="flex-1 flex gap-4 justify-between flex-wrap font-semibold">
+                {/* CARD */}
+                <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                    <Image src="/singleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+                    <div className="">
+                        <h1 className="text-xl font-semibold">90%</h1>
+                        <span className="text-sm to-gray-400">Kehadiran</span>
+                    </div>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image src="/phone.png" alt="" width={14} height={14} />
-                  <span>+62 852 9833</span>
+                {/* CARD */}
+                <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                    <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6" />
+                    <div className="">
+                        <h1 className="text-xl font-semibold">2</h1>
+                        <span className="text-sm to-gray-400">Cuti</span>
+                    </div>
                 </div>
-              </div>
+                {/* CARD */}
+                <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                    <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6" />
+                    <div className="">
+                        <h1 className="text-xl font-semibold">6</h1>
+                        <span className="text-sm to-gray-400">Lembur</span>
+                    </div>
+                </div>
+                {/* CARD */}
+                <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                    <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6" />
+                    <div className="">
+                        <h1 className="text-xl font-semibold">6</h1>
+                        <span className="text-sm to-gray-400">Izin - Sakit</span>
+                    </div>
+                </div>
             </div>
-          </div>
-          {/* User Info Small Card */}
-          <div className="flex-1 flex gap-4 justify-between flex-wrap">
-            {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleAttendance.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">90%</h1>
-                <span className="text-sm to-gray-400">Attendance</span>
-              </div>
-            </div>
-            {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleBranch.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">2</h1>
-                <span className="text-sm to-gray-400">Branches</span>
-              </div>
-            </div>
-            {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleLesson.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm to-gray-400">Lessons</span>
-              </div>
-            </div>
-            {/* CARD */}
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-              <Image
-                src="/singleClass.png"
-                alt=""
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              />
-              <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm to-gray-400">Classes</span>
-              </div>
-            </div>
-          </div>
         </div>
         {/* Profil Bagian Bawah */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-            <h1>Teacher&apos;s Schedule</h1>
+            <h1 className="font-semibold">Rekapitulasi Kehadiran Pegawai</h1>
+            <BigCalendar />
         </div>
-      </div>
-      {/* Bagian Kanan */}
-      <div className="w-full xl:w-1/3">r`</div>
     </div>
-  );
+    {/* Bagian Kanan */}
+    <div className="w-full xl:w-1/3 flex flex-col gap-4">
+    <div className="bg-white p-4 rounded-md">
+      <h1 className="text-xl font-semibold">Shortcuts</h1>
+      <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+        <Link className="p-3 rounded-md bg-lamaSkyLight" href="/absen">Absensi</Link>
+        <Link className="p-3 rounded-md bg-LamaPurpleLight" href="/cuti">Pengajuan Cuti</Link>
+        <Link className="p-3 rounded-md bg-LamaYellowLight" href="/lembur">Pengajuan Lembur</Link>
+        <Link className="p-3 rounded-md bg-lamaSky" href="/download">Download</Link>
+      </div>
+    </div>
+    </div>
+</div>
+);
 };
 
 export default SinglePegawaiPage;
