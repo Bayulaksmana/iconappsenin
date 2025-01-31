9593ea0d12a412fcfc0c4d69dbbb37327468c821
Mosagol <bayu.laksmana14@gmail.com>
2025-01-31 13:31:49 +0700

    Update Form Modal Delete

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const connectionPool = new Pool({
-connectionString: process.env.POSTGRES_URL,
-user: process.env.POSTGRES_USER,
-host: process.env.POSTGRES_HOST,
-database: process.env.POSTGRES_DATABASE,
-password: process.env.POSTGRES_PASSWORD,
-port: process.env.NODE_ENV,
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-
-module.exports = pool;
+export default pool;
